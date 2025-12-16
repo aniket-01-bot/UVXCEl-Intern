a8c3abb1719bce33a0d26dbcc6174d58b9e3b242
Aniket Shinde <161941144+aniket-01-bot@users.noreply.github.com>
2025-12-16 10:53:48 +0530

    Add files via upload

---
+++ L.T 1.py
@@ -0,0 +1,23 @@
+log_file = "C:\\Users\\DELL\\OneDrive\\Desktop\\uvXcel\\task-1\\task-1\\large.log"
+
+InfoCount = 0
+WarnCount = 0
+ErrorCount = 0
+Total = 0
+
+with open(log_file, "r") as f:
+    for line in f:
+        Total += 1
+        line_upper = line.upper()
+
+        if "INFO" in line_upper:
+            InfoCount += 1
+        elif "WARN" in line_upper:
+            WarnCount += 1
+        elif "ERROR" in line_upper:
+            ErrorCount += 1
+
+print("Total entries:", Total)
+print("INFO:", InfoCount)
+print("WARN:", WarnCount)
+print("ERROR:", ErrorCount)
---
+++ L.t 2.py
@@ -0,0 +1,18 @@
+log_file = "C:\\Users\\DELL\\OneDrive\\Desktop\\uvXcel\\task-1\\task-1\\large.log"
+
+search_text = input("Enter text to search in log file: ").strip().lower()
+
+count = 0
+
+file = open(log_file, "r")
+
+line_no = 1
+for line in file:
+    if search_text in line.lower():   # case-insensitive search
+        print("Line", line_no, ":", line.strip())
+        count = count + 1
+    line_no = line_no + 1
+
+file.close()
+
+print("\nTotal matches:", count)
